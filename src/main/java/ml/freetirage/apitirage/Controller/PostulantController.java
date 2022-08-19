701733070b0cf614c2b585c7f4cd61c1ba2b1936
dadiallo <d.abdoulayead28@gmail.com>
2022-08-19 10:31:13 +0000

    ajout de controller

--- src/main/java/ml/freetirage/apitirage/Controller/PostulantController.java
+++ src/main/java/ml/freetirage/apitirage/Controller/PostulantController.java
@@ -4,14 +4,33 @@ import lombok.AllArgsConstructor;
 import ml.freetirage.apitirage.Modele.Postulant;
 import ml.freetirage.apitirage.Service.PostulantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/")
+@RequestMapping("/Postulant")
 public class PostulantController {
     @Autowired
     PostulantService postulantService;
+    @PostMapping("/Ajout")
+    public Postulant ajout(@RequestBody Postulant postulant){
+        return postulantService.Ajouter(postulant);
+    }
+    @GetMapping("/Afficher")
+    public List<Postulant> Afficher(){
+        return postulantService.Afficher();
+    }
+
+    @PutMapping("/Modifier")
+    public Postulant Supprimer(@PathVariable Long id_postulant,@RequestBody Postulant postulant){
+        return postulantService.Modifier(postulant,id_postulant);
+
+    }
+    @DeleteMapping("/Supprimer")
+    public String Supprimer(@PathVariable Long id_postulant){
+     return postulantService.Supprimer(id_postulant);
+    }
 
 }
