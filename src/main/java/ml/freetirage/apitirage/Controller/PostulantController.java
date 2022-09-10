7a097e44c18ce06b6e70068f86b3865f8a29107b
dadiallo <d.abdoulayead28@gmail.com>
2022-09-10 16:44:52 +0000

    liason à angular

--- src/main/java/ml/freetirage/apitirage/Controller/PostulantController.java
+++ src/main/java/ml/freetirage/apitirage/Controller/PostulantController.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/API/Postulant")
