15e5930ee620b8d89832accfde2a55a431c44ff2
dadiallo <d.abdoulayead28@gmail.com>
2022-09-16 17:54:38 +0000

    reglage probleme d'accès

--- src/main/java/ml/freetirage/apitirage/Controller/PostulantTireController.java
+++ src/main/java/ml/freetirage/apitirage/Controller/PostulantTireController.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 
 import ml.freetirage.apitirage.Model.PostulantTire;
 import ml.freetirage.apitirage.Service.PostulantTireService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/PostulantTire")
